40597f88f2d1013b9b54003725fbe1b0a9a79f17
VRAraya <victor.arayar@utem.cl>
2019-10-02 14:33:50 -0300

    Commit preventivo por cambio de rama

--- sockets/Server.java
+++ sockets/Server.java
@@ -7,55 +7,58 @@ import java.util.*;
 import java.net.*; 
   
 // Clase Servidor
-public class Server  
-{ 
-  
-    // Vector para almacenar los usuarios activos
-    static Vector<ClientHandler> ar = new Vector<>(); 
-      
-    // Contador de clientes
-    static int i = 0; 
-  
-    public static void main(String[] args) throws IOException  
-    { 
-        // Server escuchará en el puerto 1234
-        ServerSocket serverSocket = new ServerSocket(1234); 
-          
-        Socket s; 
-          
-        // Corre en loop infinito para escuchar solicitudes de clientes
+public class Server
+{
+  // Vector para almacenar los usuarios activos
+  static Vector<ClientHandler> ar = new Vector<>();
 
-        while (true)  
-        { 
-            // Aceptar la solicitud entrante
-            s = serverSocket.accept(); 
+  // Contador de clientes
+  static int i = 0;
+  public static void main(String[] args) throws IOException  
+  {
+    Scanner scn = new Scanner(System.in);
+    
+    System.out.println("Configuración del servidor");
+    System.out.println("Ingrese el puerto a escuchar: ");
+    Integer port = scn.nextInt();
+
+    // Server escuchará en el puerto port
+    ServerSocket serverSocket = new ServerSocket(port);
+
+    Socket s;
+
+    // Corre en loop infinito para escuchar solicitudes de clientes
+
+    while (true)
+    {
+      // Aceptar la solicitud entrante
+      s = serverSocket.accept();
+
+      // Se declaran los flujos de entrada y salida para trabajar con ellos
+      DataInputStream inStream = new DataInputStream(s.getInputStream()); 
+      DataOutputStream outStream = new DataOutputStream(s.getOutputStream()); 
+
+      //Se notifica dentro del servidor que se ha conectado un usuario
+      System.out.println("Se ha conectado un cliente : " + s); 
+
+      // Se crea un nuevo manejador de clientes para encargarse de este. 
+      ClientHandler mtch = new ClientHandler(s,"client " + i, inStream, outStream); 
   
-            System.out.println("Se ha conectado un cliente : " + s); 
-              
-            // Se declaran los flujos de entrada y salida para trabajar con ellos
-            DataInputStream inStream = new DataInputStream(s.getInputStream()); 
-            DataOutputStream outStream = new DataOutputStream(s.getOutputStream()); 
-              
-            // Se crea un nuevo manejador de clientes para encargarse de este. 
-            ClientHandler mtch = new ClientHandler(s,"client " + i, inStream, outStream); 
+      // Crear un nuevo hilo con este objeto 
+      Thread t = new Thread(mtch);
+
+      System.out.println("Se ingresa a la lista de usuarios activos"); 
+      // Se agrega a la lista de usuarios activos
+      ar.add(mtch); 
   
-            // Crear un nuevo hilo con este objeto 
-            Thread t = new Thread(mtch); 
-              
-            System.out.println("Se ingresa a la lista de usuarios activos"); 
+      // Se inicia el hilo
+      t.start(); 
   
-            // Se agrega a la lista de usuarios activos
-            ar.add(mtch); 
-  
-            // Se inicia el hilo
-            t.start(); 
-  
-            // Se suma 1 al conteo de usuarios
-            // se usa sólo para nombrar usuarios, pero puede ser reemplazado
-            // por cualquier esquema de nombres
-            i++; 
-  
-        } 
-    } 
-} 
-  
+      // Se suma 1 al conteo de usuarios
+      // se usa sólo para nombrar usuarios, pero puede ser reemplazado
+      // por cualquier esquema de nombres
+      i++; 
+
+    }
+  }
+}
